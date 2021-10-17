20bd88bc3e8043bfad57047b0258b9ce3a477263
Tien <tpham7459@gmail.com>
2021-10-18 04:42:15 +0700

    Premium Form, SEO

--- resource/main.js
+++ resource/main.js
@@ -1,30 +1,42 @@
 function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      toTop.style.display = "block";
-      menu.style.background = "white";
-    } else {
-      toTop.style.display = "none";
-      menu.style.background  = "none";;
-    }
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    toTop.style.display = "block";
+    menu.style.background = "white";
+
+  } else {
+    toTop.style.display = "none";
+    menu.style.background = "none";
+    menu.style.color = "red";
   }
+}
 
 function topFunction() {
-    document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function check() {
+  let name = document.getElementById("user").value;
+  let pwd = document.getElementById("password").value;
+  let wrn = document.getElementById("noti");
+  if (name !== "admin@gmail.com" || pwd !== "123") {
+    wrn.style.display = "block";
+    return false;
   }
+}
 
-  function check() 
-    {
-        let name = document.getElementById("user").value;
-        let pwd = document.getElementById("password").value;
-        let wrn = document.getElementById("noti");
-        if (name !== "admin@gmail.com" || pwd !== "123")
-        { 
-          wrn.style.display = "block";
-          return false;
-        }
-    }
+function toPage1() {
+  pg1.style.display = "flex";
+  pg2.style.display = "none";
+  btn1.classList.add("active");
+  btn2.classList.remove("active");
+}
 
-    
+function toPage2() {
+  pg2.style.display = "flex";
+  pg1.style.display = "none";
+  btn2.classList.add("active");
+  btn1.classList.remove("active");
+}
