b780fd3fd936a0d1cd3fe3e412012d73c177be58
thanhphn2002 <phnthanh2002@gmail.com>
2022-01-23 04:25:15 +0700

    Responsive

--- resource/main.js
+++ resource/main.js
@@ -1,16 +1,105 @@
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    toTop.style.display = "block";
-    menu.style.background = "white";
 
-  } else {
-    toTop.style.display = "none";
-    menu.style.background = "none";
-    menu.style.color = "red";
+$(function()
+{
+  AOS.init({ duration: 1500 }); // Thời gian chạy animation
+
+  var toTop = document.getElementById("toTopBtn"); // Quay lại đầu trang
+  var menu = document.getElementById("menu");
+  var counted = false; // Đếm số
+
+  // Hiệu ứng Typing cho Premium
+  var typed = new Typed('.typing',
+  {
+    strings: ["Trial", "Paid", "VIP"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  })
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      toTop.style.display = "block";
+      menu.style.background = "white";
+  
+    } else {
+      toTop.style.display = "none";
+      menu.style.background = "none";
+      menu.style.color = "red";
+    }
   }
+  
+  // Kéo đến phần Gaming mới bắt đầu đếm
+  window.onscroll = function () {
+    scrollFunction();
+
+    if (window.scrollY > 1000 && !counted) {
+      $('.counter-count').each(function () {
+        $(this).animate
+          ({
+            Counter: $(this).text()
+          },
+            {
+              duration: 2500,
+              easing: 'swing',
+              step: function (now) {
+                $(this).text(Math.ceil(now));
+              }
+            });
+      });
+      counted = true;
+    }
+  };
+
+  // Hiệu ứng cho Follow trên menu
+  $(".dropdown-toggle").mouseenter(function()
+  {
+    $(".dropdown-menu").fadeIn(1000);
+  })
+
+  $(".dropdown-toggle").mouseleave(function()
+  {
+    $(".dropdown-menu").delay(1500).fadeOut(1000);
+  })
+  
+  // Hiệu ứng cho khung đăng ký cho Premium
+  $("#premium .card:first").mouseenter(function()
+  {
+    $("#premium .card:first").animate({top: '-=15px'});
+  })
+
+  $("#premium .card:first").mouseleave(function()
+  {
+    $("#premium .card:first").animate({top: '+=15px'});
+  })
+
+  $('.navbar-responsive').click(function() {
+    $('.navbar-side').css('display', 'block')
+  });
+
+  $('.navbar-side .btn').click(function() {
+    $('.navbar-side').css('display', 'none')
+  });
+
+  $('a').click(function() {
+    $('.navbar-side').css('display', 'none')
+  });
+})
+
+function toPage1() {
+  pg1.style.display = "flex";
+  pg2.style.display = "none";
+  btn1.classList.add("active");
+  btn2.classList.remove("active");
+}
+
+function toPage2() {
+  pg2.style.display = "flex";
+  pg1.style.display = "none";
+  btn2.classList.add("active");
+  btn1.classList.remove("active");
 }
 
 function topFunction() {
@@ -25,18 +114,4 @@ function check() {
     wrn.style.display = "block";
     return false;
   }
-}
-
-function toPage1() {
-  pg1.style.display = "flex";
-  pg2.style.display = "none";
-  btn1.classList.add("active");
-  btn2.classList.remove("active");
-}
-
-function toPage2() {
-  pg2.style.display = "flex";
-  pg1.style.display = "none";
-  btn2.classList.add("active");
-  btn1.classList.remove("active");
 }
